269a5f393b9631dc445739a81801b356c126a288
Nicholas Perez-Aguilar <npere017@ucr.edu>
2021-10-30 02:59:33 -0700

    Initial Code being pushed to repository.

---
+++ venv/Globals.py
@@ -0,0 +1,16 @@
+# Title: 13 Days of Kill-mas
+# Author: Nicholas Perez-Aguilar
+# File: Globals.py
+
+ONE = "A victory in Call of Duty"
+TWO = "Gun Game"
+THREE = "Gunfight"
+FOUR = "Free For All"
+FIVE = "Search & Destroy"
+SIX = "Domination"
+SEVEN = "Team Deathmatch"
+EIGHT = "Shipment"
+NINE = "Champion Hill"
+TEN = "Kill Confirmed"
+ELEVEN = "Patrol"
+TWELVE = "Bots"
---
+++ venv/main.py
@@ -0,0 +1,8 @@
+# Title: 13 Days of Kill-mas
+# Author: Nicholas Perez-Aguilar
+# File: Main.py
+
+# Globals
+
+if __name__ == "__main__":
+    pass
